2edff63eb280e44aebc299a7e97c7949a4a17312
V._.Malviya <vaibhav.cool460440@gmail.com>
2023-11-11 18:49:22 +0530

    Used InMemoryUserDetailManager to create multiple user

--- src/main/java/com/vaibhav/eazybank/configuration/ProjectSecurityConfig.java
+++ src/main/java/com/vaibhav/eazybank/configuration/ProjectSecurityConfig.java
@@ -5,6 +5,9 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.annotation.Order;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 import static org.springframework.security.config.Customizer.withDefaults;
@@ -34,5 +37,20 @@ public class ProjectSecurityConfig {
         http.formLogin(withDefaults());
         http.httpBasic(withDefaults());
         return http.build();
+    }
+
+    @Bean
+    public InMemoryUserDetailsManager userDetailsManager()
+    {
+        UserDetails u1= User.withDefaultPasswordEncoder()
+                .username("Admin")
+                .password("12345")
+                .authorities("admin").build();
+        UserDetails u2=User.withDefaultPasswordEncoder()
+                .username("User")
+                .password("12345")
+                .authorities("read")
+                .build();
+        return new InMemoryUserDetailsManager(u1,u2);
     }
 }
